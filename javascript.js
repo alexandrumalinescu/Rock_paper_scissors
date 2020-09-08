2655422a398be437dbfd3506dc91c5b17d1e802b
alexandrumalinescu <almalinescu@yahoo.com>
2020-09-08 16:48:07 +0300

    modificari

---
+++ javascript.js
@@ -0,0 +1,21 @@
+let myArray = ["rock", "paper", "scissors"];
+
+function pick(arr) {
+  const idx = Math.floor(Math.random() * arr.length);
+  // console.log(arr[idx]);
+  return arr[idx]; 
+}
+let computer = pick(myArray);
+let user = pick(myArray);
+let play = function () {
+  if (computer === user) {
+    return "It's a Tie";
+  } else if (computer === "paper" && user === "rock" || computer === "rock" && user === "scissors" || computer === "scissors" && user === "paper" ) {
+    return "Computer wins";
+  } else if (computer === "paper" && user === "scissors" || computer === "rock" && user === "paper" || computer === "scissors" && user === "rock" ) {
+    return "User wins";
+  } 
+};
+console.log("Computer choise: " + computer);
+console.log("User choice: " + user);
+console.log(play());
